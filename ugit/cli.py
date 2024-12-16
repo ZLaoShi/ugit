b7548fd4b4518fde228fd81d1c5c4073dd14c6d1
mxwj <z2116344865@gmail.com>
2024-12-16 08:53:25 +0800

    初始化，完成调试模式安装，cli输入，简易哈希

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,22 +1,34 @@
 import argparse
+import os
 
+from . import data
 
-def main ():
-    args = parse_args ()
-    args.func (args)
+def main():
+    args = parse_args()
+    # 调用绑定的 func
+    args.func(args)
+    
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-
-def parse_args ():
-    parser = argparse.ArgumentParser ()
-
-    commands = parser.add_subparsers (dest='command')
+    commands = parser.add_subparsers(dest='command')
     commands.required = True
 
-    init_parser = commands.add_parser ('init')
-    init_parser.set_defaults (func=init)
+    # 定义 init 子命令
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)  # 绑定 func
 
-    return parser.parse_args ()
+     # hash-object 子命令
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
 
+    return parser.parse_args()
 
-def init (args):
-    print ('Hello, World!')
+def init(args):
+    data.init()
+    print(f'Initialized empty ugit repository in {os.getcwd}/{data.GIT_DIR}')
+
+def hash_object(args):
+    with open (args.file, 'rb') as f:
+        print(data.hash_object(f.read()))

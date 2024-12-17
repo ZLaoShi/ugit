a3f0671c49ed40067149fd383d07ca0b56ceac02
mxwj <z2116344865@gmail.com>
2024-12-17 17:16:29 +0800

    实现了基本的生成版本，以及根据oid选择版本

---
+++ ugit/base.py
@@ -0,0 +1,62 @@
+import os
+
+from . import data
+
+def write_tree(directory='.'):
+    entries = []
+    with os.scandir(directory) as it:
+        for entry in it:
+            full = f'{directory}/{entry.name}'
+            # 增强检查：同时检查 .git 和 .ugit
+            if is_ignored(full) or '.git' in full.split('/'):
+                continue
+
+            if entry.is_file(follow_symlinks=False):
+                type_ = 'blob'
+                with open(full, 'rb') as f:
+                    oid = data.hash_object(f.read())
+            elif entry.is_dir(follow_symlinks=False):
+                type_ = 'tree'
+                oid = write_tree(full)
+            entries.append((entry.name, oid, type_))
+
+    tree = ''.join(f'{type_} {oid} {name}\n'
+                   for name, oid, type_
+                   in sorted(entries))
+    return data.hash_object(tree.encode(), 'tree')
+  
+def _iter_tree_enrties(oid):
+    if not oid:
+        return
+    tree = data.get_object(oid, 'tree')
+    for entry in tree.decode().splitlines():
+        type_, oid, name = entry.split(' ', 2)
+        yield type_, oid, name
+
+def get_tree(oid, base_path=''):
+    result = {}
+    for type_, oid, name in _iter_tree_enrties(oid):
+        assert '/' not in name
+        assert name not in ('..', '.')
+        path = base_path + name
+        if type_ == 'blob':
+            result[path] = oid
+        elif type_ == 'tree':
+            result.update(get_tree(oid, f'{path}/'))
+        else:
+            assert False, f'Unknown tree entry {type_}'
+    return result
+
+def read_tree(tree_oid):
+    for path, oid in get_tree(tree_oid, base_path='./').items():
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'wb') as f:
+            f.write(data.get_object(oid))
+
+# linux 下的隐藏文件
+def is_ignored(path):
+    return '.ugit' in path.split('/')
+
+# win 下的隐藏文件
+# def is_ignored(path):
+#     return '.ugit' in path.replace('\\', '/').split('/')
--- ugit/cli.py
+++ ugit/cli.py
@@ -1,7 +1,9 @@
 import argparse
 import os
+import sys
 
 from . import data
+from . import base
 
 def main():
     args = parse_args()
@@ -14,14 +16,27 @@ def parse_args():
     commands = parser.add_subparsers(dest='command')
     commands.required = True
 
-    # 定义 init 子命令
+    # init 子命令
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)  # 绑定 func
 
-     # hash-object 子命令
+    # hash-object 子命令
     hash_object_parser = commands.add_parser('hash-object')
     hash_object_parser.set_defaults(func=hash_object)
     hash_object_parser.add_argument('file')
+
+    # cat-file 子命令
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
+
+    # write-tree 子命令
+    write_tree_parser = commands.add_parser('write-tree')
+    write_tree_parser.set_defaults(func=write_tree)
+
+    read_tree_parser = commands.add_parser('read-tree')
+    read_tree_parser.set_defaults(func=read_tree)
+    read_tree_parser.add_argument('tree')
 
     return parser.parse_args()
 
@@ -32,3 +47,13 @@ def init(args):
 def hash_object(args):
     with open (args.file, 'rb') as f:
         print(data.hash_object(f.read()))
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object, expected=None))
+
+def write_tree(args):
+    print(base.write_tree())
+
+def read_tree(args):
+    base.read_tree(args.tree)

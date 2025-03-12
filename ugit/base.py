9e1c5cc3d738bc7e6fcd654a804f57fcc79bf764
mxwj <z2116344865@gmail.com>
2025-03-12 15:59:15 +0000

    测试三方合并2

--- ugit/base.py
+++ ugit/base.py
@@ -103,7 +103,7 @@ def read_tree(tree_oid):
 def read_tree_merged(t_base, t_HEAD, t_other):
     _empty_current_directory()
     for path, blob in diff.merge_trees(
-            get_tree(t_base), get_tree(HEAD), get_tree(t_other)).items():
+            get_tree(t_base), get_tree(t_HEAD), get_tree(t_other)).items():
         os.makedirs(f'./{os.path.dirname(path)}', exist_ok=True)
         with open(path, 'wb') as f:
             f.write(blob)
